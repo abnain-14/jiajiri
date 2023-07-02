b47e47452721c7f9c4f7d9e72d36031379c0a4a4
asyaibrahim <asyaothman14@gmail.com>
2023-07-02 12:55:48 +0300

    punda

--- resources/views/layouts/admin_app.blade.php
+++ resources/views/layouts/admin_app.blade.php
@@ -13,7 +13,7 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Digital mapping</title>
+    <title>Jiajiri</title>
 </head>
 
 <style>
